dc54f4de01c872f7df33bccbab9b7316534f1a2c
Rafael de Carvalho Gomes <82329108@ulife.com.br>
2025-10-03 12:00:49 -0300

    feat: create user service

--- user-service/userService.js
+++ user-service/userService.js
@@ -1,15 +1,52 @@
+require('dotenv').config();
 const express = require('express');
+const moogose = require('mongoose');
 const axios = require('axios');
 
 const app = express();
 app.use(express.json());
 
-app.post('/usuarios', async (req, res) => {
-const usuario = req.body;
+const MONGO_URI = process.env.MONGO_URI 
+if (!MONGO_URI) { 
+    throw new Error("MONGO_URI is not defined in environment variables");
+}
 
-await axios.post('http://localhost:4000/pedidos', {userId: usuario.id});
-res.send({message: 'Usuário criado com sucesso!', usuario});
-});
-app.listen(3000, () => {
-console.log('User Service rodando na porta 3000');
-});
+moogose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+    const userSchema = new moogose.Schema({
+        name : {type: String, default: '',},
+        email: {type: String, required: true, unique: true},
+        createdAt: {type: Date, default: Date.now}
+    });
+
+    const Order = moogose.model('User', userSchema);
+
+    app.post('/usuarios', async (req, res) => {
+        try{
+            const {name = '', email} = req.body;
+            if(!email)
+                return res.status(400).json({ error: ' User email is required'});
+
+                const user= new User({name, email});
+                const saved = await order.save();
+
+                try{
+                    await axios.post('https://localhost:4000/pedidos', {userId: saved._id, items: [], total: 0});
+                }catch (err){
+                    console.error('Erro ao criar pedido para usuario', err);
+                }
+                return res.status(201).json(saved);
+            }catch(err){
+                console.error(err)
+                if (err.code === 11000) return res.status(400).json
+                ({error: ' Email ja cadastrado'})
+           
+            return res.status(500).json({error: 'Erro ao criar usuario'})
+            }
+            });
+
+            app.listen(3000, () =>{
+                console.log('Ordre service running on port 3000');
+            });
