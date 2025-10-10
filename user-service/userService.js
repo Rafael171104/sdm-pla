3373e587914f7568f77d044739bd6d4621d609ee
Rafael de Carvalho Gomes <82329108@ulife.com.br>
2025-10-10 11:49:07 -0300

    feat: add GET users

--- user-service/userService.js
+++ user-service/userService.js
@@ -21,19 +21,19 @@ moogose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         createdAt: {type: Date, default: Date.now}
     });
 
-    const Order = moogose.model('User', userSchema);
+    const User = moogose.model('User', userSchema);
 
     app.post('/usuarios', async (req, res) => {
         try{
             const {name = '', email} = req.body;
             if(!email)
-                return res.status(400).json({ error: ' User email is required'});
+                return res.status(400).json({ error: 'User email is required'});
 
-                const user= new User({name, email});
-                const saved = await order.save();
+                const user = new User({name, email});
+                const saved = await user.save();
 
                 try{
-                    await axios.post('https://localhost:4000/pedidos', {userId: saved._id, items: [], total: 0});
+                    await axios.post('http://localhost:4000/pedidos', {userId: saved._id, items: [], total: 0});
                 }catch (err){
                     console.error('Erro ao criar pedido para usuario', err);
                 }
@@ -47,6 +47,29 @@ moogose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
             }
             });
 
+//listar usuarios READ CRUD
+app.get('/usuarios', async (req, res) => {
+    try {
+        const users = await User.find().sort({ createdAt: -1});
+        return res.json(users);
+    } catch (err){
+        console.error(err);
+        return res.status(500).json ({error: 'Erro ao buscar usuários'});
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
             app.listen(3000, () =>{
-                console.log('Ordre service running on port 3000');
+                console.log('User service running on port 3000');
             });
